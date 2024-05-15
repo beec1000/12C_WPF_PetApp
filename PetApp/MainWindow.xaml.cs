02e092483707eb769a961cc2106159a7d030d2f4
beec1000 <beec1000.minecraft@gmail.com>
2024-05-15 11:33:25 +0200

    Add project files.

---
+++ PetApp/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PetApp
+{
+    public partial class MainWindow : Window
+    {
+        List<Pet> pets = new List<Pet>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            pets = Pet.FromFile(@"..\..\..\src\animals.txt");
+
+            var sortedPets = new List<string>();
+            foreach (var i in pets)
+            {
+                sortedPets.Add(i.ToString());
+            }
+            sortedPets.Sort();
+
+            List.ItemsSource = sortedPets;
+            List.SelectedItem = sortedPets[0];
+
+        }
+
+        private void AddToFavoriteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = List.SelectedItem;
+
+            if (selected != null)
+            {
+                FavoriteList.Items.Add(selected);
+            }
+        }
+
+        private void DeleteFromFavoriteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = List.SelectedItem;
+            if (selected != null && FavoriteList.Items.Contains(selected))
+            {
+                FavoriteList.Items.Remove(selected);
+            }
+            else
+            {
+                MessageBox.Show("This pet is not in Favorites!", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ PetApp/Pet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace PetApp
+{
+    internal class Pet
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+        public string ImgS { get; set; }
+
+        public Pet(string name, int age, string color, string imgS)
+        {
+            Name = name;
+            Age = age;
+            Color = color;
+            ImgS = $"IMAGES/{imgS}.jpg";
+        }
+
+        public static List<Pet> FromFile(string path)
+        {
+            List<Pet> pets = new List<Pet>();
+
+            string[] line = File.ReadAllLines(path);
+
+            foreach ( var l in line )
+            {
+                string[] p = l.Split(';');
+
+                string PetName = p[0];
+                int PetAge = int.Parse(p[1]);
+                string PetColor = p[2];
+                string PetImgS = p[3];
+
+                Pet pet = new Pet(PetName, PetAge, PetColor, PetImgS);
+                pets.Add(pet);
+            }
+            return pets;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} | {Age} | {Color} {ImgS}";
+        }
+    }
+}
